cd2b954abb1e5295922ff38ac9d98e4640f95cb7
seoilgun <seoilgun@gmail.com>
2024-02-12 18:16:06 +0900

    gg

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -25,14 +25,9 @@ def compute_fig_matrix(pos, frames_pos, n_frames):
 
 
 def compute_var_matrix(pos, frames_pos, n_frames):
-    print("pos.shape", pos.shape)
-    print(frames_pos.shape)
-    print(frames_pos)
-    print(n_frames)
     pose = np.zeros(shape=(pos.shape[0], n_frames))
 
     for n in range(pose.shape[0]):
-        print("frames_pos[n]", frames_pos[n])
         pose[n, frames_pos[n] - 1] = pos[n]
 
     return pose
