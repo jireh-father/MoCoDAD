c9d9e786d3bee28e294dafbd84be8b31f807c5ba
seoilgun <seoilgun@gmail.com>
2024-06-20 20:00:48 +0900

    history

--- eval_MoCoDAD.py
+++ eval_MoCoDAD.py
@@ -8,6 +8,7 @@ from models.mocodad_latent import MoCoDADlatent
 from utils.argparser import init_args
 from utils.dataset import get_dataset_and_loader
 from utils import slack
+import pickle
 
 
 if __name__== '__main__':
@@ -39,6 +40,13 @@ if __name__== '__main__':
         trainer = pl.Trainer(accelerator=args.accelerator, devices=args.devices[:1],
                              default_root_dir=args.ckpt_dir, max_epochs=1, logger=False)
         out = trainer.test(model, dataloaders=loader, ckpt_path=ckpt_path)
+
+        clip_fname_pred_map = model.best_metrics['clip_fname_pred_map']
+        pickle.dump(clip_fname_pred_map, open(os.path.join(args.ckpt_dir, 'eval_clip_fname_pred_map.pkl'), 'wb+'))
+        # np.save(os.path.join(args.ckpt_dir, 'clip_fname_pred_map.npy'), clip_fname_pred_map)
+        del model.best_metrics['clip_fname_pred_map']
+        pickle.dump(model.best_metrics, open(os.path.join(args.ckpt_dir, 'eval_best_metrics.pkl'), 'wb+'))
+        print(model.best_metrics)
 
     if slack_webhook_url:
         keys = ['clip_auc', 'auc', 'f1', 'recall', 'precision', 'best_thr', 'ori_clip_auc', 'ori_auc']
