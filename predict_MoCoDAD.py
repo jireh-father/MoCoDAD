8580e7ec72cdae45a757b357bc8481259e7286ce
seoilgun <seoilgun@gmail.com>
2024-10-01 13:45:14 +0900

    history

--- predict_MoCoDAD.py
+++ predict_MoCoDAD.py
@@ -9,7 +9,6 @@ from utils.argparser import init_args
 from utils.dataset import get_dataset_and_loader
 from utils.model_utils import processing_data
 
-
 # Parse command line arguments and load config file
 parser = argparse.ArgumentParser(description='MoCoDAD')
 parser.add_argument('-c', '--config', type=str, required=True)
@@ -23,7 +22,7 @@ model = MoCoDAD(args)
 
 print('Loading data and creating loaders.....')
 ckpt_path = os.path.join(args.ckpt_dir, args.load_ckpt)
-dataset, loader, _, _ = get_dataset_and_loader(args, split=args.split)
+_, _, dataset, loader = get_dataset_and_loader(args, split=args.split)
 
 # Initialize trainer and test
 trainer = pl.Trainer(accelerator=args.accelerator, devices=args.devices,
@@ -31,6 +30,7 @@ trainer = pl.Trainer(accelerator=args.accelerator, devices=args.devices,
 out = trainer.predict(model, dataloaders=loader, ckpt_path=ckpt_path, return_predictions=True)
 unpacked_result = processing_data(out)
 file_names = ['prediction', 'gt_data', 'trans', 'metadata', 'frames']
-save_dir = os.path.join(args.ckpt_dir, f'saved_tensors_{args.split}_{args.aggregation_strategy}_{args.n_generated_samples}')
+save_dir = os.path.join(args.ckpt_dir,
+                        f'saved_tensors_{args.split}_{args.aggregation_strategy}_{args.n_generated_samples}')
 for i in range(len(unpacked_result)):
-    torch.save(unpacked_result[i], os.path.join(save_dir, file_names[i]+'.pt'))
+    torch.save(unpacked_result[i], os.path.join(save_dir, file_names[i] + '.pt'))
