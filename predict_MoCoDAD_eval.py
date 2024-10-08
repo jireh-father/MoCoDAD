316fe8726594978a565d4e70f180e9a38b5e0e87
seoilgun <seoilgun@gmail.com>
2024-10-08 15:34:17 +0900

    df

--- predict_MoCoDAD_eval.py
+++ predict_MoCoDAD_eval.py
@@ -56,7 +56,7 @@ def main(args, tmp_dir, data_json, keypoint_dir):
     # Initialize the model
     model = MoCoDAD(args)
     checkpoint = torch.load(args.load_ckpt, weights_only=True)
-    print(checkpoint)
+    print(checkpoint.keys())
     model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
     model.to('cuda')
